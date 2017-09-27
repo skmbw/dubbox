4216506b7c362d01d8bd34cb596e9c4aee510157
skmbw <tongku2008@126.com>
2017-09-27 11:15:48 +0800

    thrift 共用同一个端口

--- dubbo-rpc/dubbo-rpc-thrift2/src/main/java/com/alibaba/dubbo/rpc/protocol/thrift2/Thrift2Protocol.java
+++ dubbo-rpc/dubbo-rpc-thrift2/src/main/java/com/alibaba/dubbo/rpc/protocol/thrift2/Thrift2Protocol.java
@@ -6,9 +6,10 @@ import com.alibaba.dubbo.common.logger.LoggerFactory;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.alibaba.dubbo.rpc.protocol.AbstractProxyProtocol;
 import org.apache.thrift.TException;
+import org.apache.thrift.TMultiplexedProcessor;
 import org.apache.thrift.TProcessor;
-import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TMultiplexedProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadedSelectorServer;
@@ -18,14 +19,21 @@ import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 
 import java.lang.reflect.Constructor;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * 为dubbo-rpc添加"原生thrift"支持
- * by 杨俊明(http://yjmyzz.cnblogs.com/)
+ * 为dubbox-rpc添加"原生thrift"支持
+ *
+ * @author yinlei
+ * @since 2017-9-27
  */
 public class Thrift2Protocol extends AbstractProxyProtocol {
     public static final int DEFAULT_PORT = 33208;
-    private static final Logger logger = LoggerFactory.getLogger(Thrift2Protocol.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Thrift2Protocol.class);
+
+    private volatile AtomicBoolean init = new AtomicBoolean(false);
+    private TMultiplexedProcessor multiplexedProcessor;
+    private TServer thriftServer;
 
     public int getDefaultPort() {
         return DEFAULT_PORT;
@@ -33,34 +41,71 @@ public class Thrift2Protocol extends AbstractProxyProtocol {
 
     public Thrift2Protocol() {
         super(TException.class, RpcException.class);
+        multiplexedProcessor = new TMultiplexedProcessor();
     }
 
+    private void init (URL url) {
+        TThreadedSelectorServer.Args tArgs = null;
+        try {
+            // 解决并发连接数上限默认只有50的问题
+            TNonblockingServerSocket.NonblockingAbstractServerSocketArgs args = new TNonblockingServerSocket.NonblockingAbstractServerSocketArgs();
+            args.backlog(1000);// 1k个连接
+            args.port(url.getPort());
+            args.clientTimeout(8000);// 10秒超时
+
+            TNonblockingServerSocket transport = new TNonblockingServerSocket(args);
+
+            tArgs = new TThreadedSelectorServer.Args(transport);
+            tArgs.workerThreads(200);
+            tArgs.selectorThreads(4);
+            tArgs.acceptQueueSizePerThread(256);
+            tArgs.processor(multiplexedProcessor);
+            tArgs.transportFactory(new TFramedTransport.Factory());
+            tArgs.protocolFactory(new TCompactProtocol.Factory());
+        } catch (Exception e) {
+            LOGGER.error("init thrift server error.", e);
+        }
+
+        if (tArgs == null) {
+            LOGGER.error("Fail to create thrift server(" + url + ") due to null args");
+            throw new RpcException("Fail to create thrift server(" + url + ") due to null args");
+        }
+        thriftServer = new TThreadedSelectorServer(tArgs);
+        Thread thread = new Thread(() -> {
+            LOGGER.info("Start Thrift ThreadedSelectorServer");
+            thriftServer.serve();
+            LOGGER.info("Thrift ThreadedSelectorServer started.");
+        });
+        thread.start();
+    }
 
     @Override
     protected <T> Runnable doExport(T impl, Class<T> type, URL url)
             throws RpcException {
-        logger.info("impl => " + impl.getClass());
-        logger.info("type => " + type.getName());
-        logger.info("url => " + url);
+        LOGGER.info("impl => " + impl.getClass());
+        LOGGER.info("type => " + type.getName());
+        LOGGER.info("url => " + url);
         return exportThreadedSelectorServer(impl, type, url);
     }
 
     @Override
     protected <T> T doRefer(Class<T> type, URL url) throws RpcException {
-        logger.info("type => " + type.getName());
-        logger.info("url => " + url);
+        LOGGER.info("type => " + type.getName());
+        LOGGER.info("url => " + url);
         return doReferFrameAndCompact(type, url);
     }
 
-
     private <T> Runnable exportThreadedSelectorServer(T impl, Class<T> type, URL url)
             throws RpcException {
+        if (init.compareAndSet(false ,true)) {
+            init(url);
+        }
         TProcessor tprocessor;
-        TThreadedSelectorServer.Args tArgs = null;
+//        TThreadedSelectorServer.Args tArgs = null;
         String iFace = "$Iface";
         String processor = "$Processor";
         String typeName = type.getName();
-        TNonblockingServerSocket transport;
+//        TNonblockingServerSocket transport;
         if (typeName.endsWith(iFace)) {
             String processorClsName = typeName.substring(0, typeName.indexOf(iFace)) + processor;
             try {
@@ -68,60 +113,58 @@ public class Thrift2Protocol extends AbstractProxyProtocol {
                 Constructor constructor = clazz.getConstructor(type);
                 try {
                     tprocessor = (TProcessor) constructor.newInstance(impl);
+                    // 共用端口
+                    multiplexedProcessor.registerProcessor(typeName, tprocessor);
 
-                    //解决并发连接数上限默认只有50的问题
-                    TNonblockingServerSocket.NonblockingAbstractServerSocketArgs args = new TNonblockingServerSocket.NonblockingAbstractServerSocketArgs();
-                    args.backlog(1000);//1k个连接
-                    args.port(url.getPort());
-                    args.clientTimeout(10000);//10秒超时
-
-                    transport = new TNonblockingServerSocket(args);
-
-                    tArgs = new TThreadedSelectorServer.Args(transport);
-                    tArgs.workerThreads(200);
-                    tArgs.selectorThreads(4);
-                    tArgs.acceptQueueSizePerThread(256);
-                    tArgs.processor(tprocessor);
-                    tArgs.transportFactory(new TFramedTransport.Factory());
-                    tArgs.protocolFactory(new TCompactProtocol.Factory());
+                    // 解决并发连接数上限默认只有50的问题
+//                    TNonblockingServerSocket.NonblockingAbstractServerSocketArgs args = new TNonblockingServerSocket.NonblockingAbstractServerSocketArgs();
+//                    args.backlog(1000);// 1k个连接
+//                    args.port(url.getPort());
+//                    args.clientTimeout(10000);// 10秒超时
+//
+//                    transport = new TNonblockingServerSocket(args);
+//
+//                    tArgs = new TThreadedSelectorServer.Args(transport);
+//                    tArgs.workerThreads(200);
+//                    tArgs.selectorThreads(4);
+//                    tArgs.acceptQueueSizePerThread(256);
+//                    tArgs.processor(tprocessor);
+//                    tArgs.transportFactory(new TFramedTransport.Factory());
+//                    tArgs.protocolFactory(new TCompactProtocol.Factory());
                 } catch (Exception e) {
-                    logger.error(e.getMessage(), e);
+                    LOGGER.error(e.getMessage(), e);
                     throw new RpcException("Fail to create thrift server(" + url + ") : " + e.getMessage(), e);
                 }
             } catch (Exception e) {
-                logger.error(e.getMessage(), e);
+                LOGGER.error(e.getMessage(), e);
                 throw new RpcException("Fail to create thrift server(" + url + ") : " + e.getMessage(), e);
             }
         }
 
-        if (tArgs == null) {
-            logger.error("Fail to create thrift server(" + url + ") due to null args");
-            throw new RpcException("Fail to create thrift server(" + url + ") due to null args");
-        }
-        final TServer thriftServer = new TThreadedSelectorServer(tArgs);
+//        if (tArgs == null) {
+//            LOGGER.error("Fail to create thrift server(" + url + ") due to null args");
+//            throw new RpcException("Fail to create thrift server(" + url + ") due to null args");
+//        }
+//        final TServer thriftServer = new TThreadedSelectorServer(tArgs);
+//
+//        Thread thread = new Thread(() -> {
+//            LOGGER.info("Start Thrift ThreadedSelectorServer");
+//            thriftServer.serve();
+//            LOGGER.info("Thrift ThreadedSelectorServer started.");
+//        });
+//        thread.start();
 
-        new Thread(new Runnable() {
-            public void run() {
-                logger.info("Start Thrift ThreadedSelectorServer");
-                thriftServer.serve();
-                logger.info("Thrift ThreadedSelectorServer started.");
-            }
-        }).start();
-
-        return new Runnable() {
-            public void run() {
-                try {
-                    logger.info("Close Thrift NonblockingServer");
-                    thriftServer.stop();
-                } catch (Throwable e) {
-                    logger.warn(e.getMessage(), e);
-                }
+        return () -> {
+            try {
+                LOGGER.info("Close Thrift NonblockingServer");
+                thriftServer.stop();
+            } catch (Throwable e) {
+                LOGGER.warn(e.getMessage(), e);
             }
         };
     }
 
     private <T> T doReferFrameAndCompact(Class<T> type, URL url) throws RpcException {
-
         try {
             TSocket tSocket;
             TTransport transport;
@@ -133,25 +176,26 @@ public class Thrift2Protocol extends AbstractProxyProtocol {
             String typeName = type.getName();
             if (typeName.endsWith(iFace)) {
                 String clientClsName = typeName.substring(0, typeName.indexOf(iFace)) + client;
-                Class<?> clazz = Class.forName(clientClsName);
-                Constructor constructor = clazz.getConstructor(TProtocol.class);
+                Class<T> clazz = (Class) Class.forName(clientClsName);
+                Constructor<T> constructor = clazz.getConstructor(TProtocol.class);
                 try {
                     tSocket = new TSocket(url.getHost(), url.getPort());
                     transport = new TFramedTransport(tSocket);
                     protocol = new TCompactProtocol(transport);
-                    thriftClient = (T) constructor.newInstance(protocol);
+                    // 共用端口
+                    TMultiplexedProtocol multiplexedProtocol = new TMultiplexedProtocol(protocol, typeName);
+                    thriftClient = constructor.newInstance(multiplexedProtocol);
                     transport.open();
-                    logger.info("thrift client opened for service(" + url + ")");
+                    LOGGER.info("thrift client opened for service(" + url + ")");
                 } catch (Exception e) {
-                    logger.error(e.getMessage(), e);
+                    LOGGER.error(e.getMessage(), e);
                     throw new RpcException("Fail to create remote client:" + e.getMessage(), e);
                 }
             }
             return thriftClient;
         } catch (Exception e) {
-            logger.error(e.getMessage(), e);
+            LOGGER.error(e.getMessage(), e);
             throw new RpcException("Fail to create remote client for service(" + url + "): " + e.getMessage(), e);
         }
     }
-
 }
