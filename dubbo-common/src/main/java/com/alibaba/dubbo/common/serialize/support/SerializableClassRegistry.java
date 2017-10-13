d59b7cb0cc1f69c5304f1864c4c629b7844b1602
skmbw <tongku2008@126.com>
2017-10-13 16:04:41 +0800

    注册的类，使用牌序的TreeSet

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/serialize/support/SerializableClassRegistry.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/serialize/support/SerializableClassRegistry.java
@@ -1,14 +1,14 @@
 package com.alibaba.dubbo.common.serialize.support;
 
-import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author lishen
  */
 public abstract class SerializableClassRegistry {
 
-    private static final Set<Class> registrations = new LinkedHashSet<Class>();
+    private static final Set<Class> registrations = new TreeSet<>((o1, o2) -> o1.getName().compareTo(o2.getName()));
 
     /**
      * only supposed to be called at startup time
