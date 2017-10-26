d12a2c03dd42ca530734e75a51dbc1e57fdc3733
skmbw <tongku2008@126.com>
2017-10-26 13:36:27 +0800

    忘记检查bytebyffer的大小了

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/serialize/support/protostuff/ProtoOutput.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/serialize/support/protostuff/ProtoOutput.java
@@ -267,7 +267,6 @@ public class ProtoOutput implements ObjectOutput {
         } else if (obj instanceof Throwable) {
             // 因为异常的超类Throwable中的cause是引用的自己，有循环引用。目前protostuff还不能处理，fst倒是可以。
             // 所以，自己简单将其序列化，只返回类名和message。
-            byteBuffer.put((byte) 16);
             byte[] nameBytes = obj.getClass().getName().getBytes();
             Throwable throwable = (Throwable) obj;
             String message = throwable.getMessage();
@@ -280,6 +279,8 @@ public class ProtoOutput implements ObjectOutput {
             if (messageBytes != null) {
                 totalLength += messageBytes.length;
             }
+            check(totalLength + 9);
+            byteBuffer.put((byte) 16);
             byteBuffer.putInt(totalLength);
             byteBuffer.putInt(nameLength);
             byteBuffer.put(nameBytes);
